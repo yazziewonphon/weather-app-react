0b0e9d47717c90db6bafac14d7e235b459c73c3f
Olive Phan <108431343+yazziewonphon@users.noreply.github.com>
2023-01-27 16:40:53 -0600

    installed react in weather app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Hello World</h1>
+      
+
+    </div>
+    </div>
+  );
+}
+
+export default App;

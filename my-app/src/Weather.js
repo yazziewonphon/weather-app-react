086f66d70e089fc1d3fdb26977fa47b250a64f7a
Olive Phan <108431343+yazziewonphon@users.noreply.github.com>
2023-01-28 17:34:26 -0600

    js boolean to return temp data from openweathermap

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Weather.css';
 import Snowflake from "./Snowflake.svg";
 
 export default function Weather() {
+    const [ready, setReady] = useState(false);
+const [temperature, setTemperature] = useState(null);
+
+    function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+}
+
+if (ready) {
     return(
         <div className="Weather">
             <form>
@@ -17,17 +28,29 @@ export default function Weather() {
 <div className="row weather-log-display">
     <div className="col-3 d-flex justify-content-start icon-and-temperature-display">
         <img src={Snowflake} alt="Snowy" className="weather-icon" />
-        <h2 className="current-temperature-display">24
+        <h2 className="current-temperature-display">{temperature}
         <span className="unit">°F</span>
         </h2>
         </div>
         <div className="col-9 d-flex justify-content-end">
             <ul>
                 <li>Precipitation: 15%</li>
-                <li>Humidity: 72%</li><li>Wind: 13 km/h</li>
+                <li>Humidity: 72%</li>
+                <li>Wind: 13 km/h</li>
                 </ul>
                 </div>
                 </div>
         </div>
     );
+} else {
+    const apiKey="b7c9ca10tacof91c4da4936c2801b27b";
+    const apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    let city="Cedar Rapids";
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+}
+
+    
+    
 }
